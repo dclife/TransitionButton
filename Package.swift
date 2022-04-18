ddd39491e3c2dec0d7ada113bcec4181b1998694
dclife <dclife2009@gmail.com>
2022-04-18 12:33:36 +0200

    update SPM

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "TransitionButton",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v9)
     ],
     products: [
         .library(
